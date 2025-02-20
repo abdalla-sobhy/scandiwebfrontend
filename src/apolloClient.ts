3937a1e632bde230b7e43483a186776060a87c02
عبدالله صبحي مصطفي محمد درويش <154272695+abdalla-sobhy@users.noreply.github.com>
2025-02-20 18:57:37 +0200

    Update apolloClient.ts

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:80/api/graphql.php',
+  uri: 'http://scandiwebtest.mooo.com/api/graphql.php',
   cache: new InMemoryCache(),
 });
 
